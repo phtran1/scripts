690523fd67b9691d8f229b83bab439460eeab7fa
Phillip Tran <tranphillip206@gmail.com>
2026-09-09 15:11:56 -0700

    Password Script with extra security

--- passwordchecker/checkmypass.py
+++ passwordchecker/checkmypass.py
@@ -1,5 +1,9 @@
 import requests
 import hashlib
+import sys
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 def request_api_data(query_char):
     url = "https://api.pwnedpasswords.com/range/" + query_char
@@ -9,10 +13,38 @@ def request_api_data(query_char):
         raise RuntimeError(f"Error fetching: {response.status_code}, check the API and try again.")
     return response
 
+def get_pw_leaks_count(hashes, hash_to_check):
+    # Matches tail with returned hashes to get our password count
+    hashes = (line.split(':') for line in hashes.text.splitlines())
+    for h, count in hashes:
+        if h == hash_to_check:
+            return count
+    return 0
+        
 def pwned_api_check(password):
     #Check Password if it exists in API response 
     sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
-    return sha1password
+    first5_char, tail = sha1password[:5], sha1password[5:]
+    response = request_api_data(first5_char)
+    return get_pw_leaks_count(response, tail)
 
-pw = pwned_api_check("123")
-request_api_data(pw[:5])
+def main():
+    raw_pw = os.getenv("secret_pw", "")
+
+    if not raw_pw:
+        print("Password not found or empty .env file")
+        return
+    
+    pw_list = raw_pw.split(',') 
+
+    # Returns how many times a password has been leaked and replaces password with * for output
+    for pw in pw_list:
+        count = pwned_api_check(pw)
+        hidden_pw = '*' * len(pw)
+        if count:
+            print(f'{hidden_pw} was found {count} times... you should probably change your password.')
+        else:
+            print(f'{hidden_pw} was NOT leaked you are safe.')
+
+if __name__ == '__main__':
+    main()
