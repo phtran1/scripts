36173c9d9f27ea0c81c4c3dbd7f6ce18a9d392f6
Phillip Tran <tranphillip206@gmail.com>
2026-09-09 16:10:09 -0700

    Email Note

--- email_playground/email_sender.py
+++ email_playground/email_sender.py
@@ -14,6 +14,9 @@ email.set_content(html.substitute({'name':'Jin'}), 'html')
 with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
     smtp.ehlo()
     smtp.starttls()
+    # Note: Gmail no longer allows regular account passwords for SMTP login.
+    # You need to generate an "App Password" from Google Account 
+    # (navigate to https://myaccount.google.com/apppasswords).
     smtp.login('example@gmail.com', 'your_password')
     smtp.send_message(email)
     print('Email sent successfully!')
